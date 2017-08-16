c9d5aeacc6baff855aed02eb1ce96c64d10d3914
Angelo Moroni <mor.angelo.mor@gmail.com>
2017-08-16 16:03:00 +0200

    Initial commit

---
+++ customfont.library/src/main/java/me/bemind/customfontlibrary/CustomFontTextView.java
@@ -0,0 +1,23 @@
+package me.bemind.customfontlibrary;
+
+import android.content.Context;
+import android.support.v7.widget.AppCompatTextView;
+import android.util.AttributeSet;
+
+/**
+ * Created by angelomoroni on 16/08/17.
+ */
+
+public class CustomFontTextView extends AppCompatTextView {
+    public CustomFontTextView(Context context) {
+        super(context);
+    }
+
+    public CustomFontTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CustomFontTextView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
---
+++ customfont.library/src/main/java/me/bemind/customfontlibrary/FontUtil.java
@@ -0,0 +1,8 @@
+package me.bemind.customfontlibrary;
+
+/**
+ * Created by angelomoroni on 16/08/17.
+ */
+
+public class FontUtil {
+}
