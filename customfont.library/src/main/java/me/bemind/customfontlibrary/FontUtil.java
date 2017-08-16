4fc3770b5b96944174606852e9fc8077b7d54a0e
Angelo Moroni <mor.angelo.mor@gmail.com>
2017-08-16 16:21:48 +0200

    enum font style to map enum xml values

--- customfont.library/src/main/java/me/bemind/customfontlibrary/FontUtil.java
+++ customfont.library/src/main/java/me/bemind/customfontlibrary/FontUtil.java
@@ -5,4 +5,23 @@ package me.bemind.customfontlibrary;
  */
 
 public class FontUtil {
+    
+    public enum FontStyle {
+        EXTRALIGHT(0), LIGHT(1), REGULAR(2),SEMIBOLD(3),
+        BOLD(4) , EXTRABOLD(5), BLACK(6),REGULAR_ITALIC(7),LIGHT_ITALIC(8),BOLD_ITALIC(9);
+        
+        int id;
+        
+        FontStyle(int id){
+            this.id = id;
+        }
+
+        public static FontStyle fromId(int id) {
+            for (FontStyle fontStyle : values()) {
+                if(fontStyle.id == id) return fontStyle;
+            }
+
+            throw new IllegalArgumentException();
+        }
+    }
 }
