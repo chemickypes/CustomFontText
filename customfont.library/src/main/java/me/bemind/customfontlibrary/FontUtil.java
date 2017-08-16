a8a6ddef99b3cb23e3dcae9b470c29a99d70e49a
Angelo Moroni <mor.angelo.mor@gmail.com>
2017-08-16 16:23:28 +0200

    list of all value of enum font style

--- customfont.library/src/main/java/me/bemind/customfontlibrary/FontUtil.java
+++ customfont.library/src/main/java/me/bemind/customfontlibrary/FontUtil.java
@@ -5,10 +5,11 @@ package me.bemind.customfontlibrary;
  */
 
 public class FontUtil {
-    
+
     public enum FontStyle {
         EXTRALIGHT(0), LIGHT(1), REGULAR(2),SEMIBOLD(3),
-        BOLD(4) , EXTRABOLD(5), BLACK(6),REGULAR_ITALIC(7),LIGHT_ITALIC(8),BOLD_ITALIC(9);
+        BOLD(4) , EXTRABOLD(5), BLACK(6),REGULAR_ITALIC(7),LIGHT_ITALIC(8),BOLD_ITALIC(9),
+        EXTRALIGHT_ITALIC(10),SEMIBOLD_ITALIC(11),EXTRABOLD_ITALIC(12),BLACK_ITALIC(13);
         
         int id;
         
